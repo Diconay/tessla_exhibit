7505f8bbcfc887c7f8a3537cac7bc6da47110bf6
Diconay <iesreg@ya.ru>
2025-08-28 17:17:57 +0700

    add 3 screens

--- main.c
+++ main.c
@@ -5,9 +5,8 @@
 
 #include "keyboard.h"
 
-#define COLOR_GRAFIT1   0x494d4e
-#define COLOR_GRAFIT2   0x3b3b48
-#define COLOR_TEXT      0x8f8f8f
+#define COLOR_GRAFIT   0x494d4e //0x3b3b48
+#define COLOR_TEXT     0x8f8f8f
 
 #define DEFAULT_FBDEV   "/dev/fb0"
 
@@ -33,25 +32,44 @@ int main(void) {
     pthread_t tick_th;
     pthread_create(&tick_th, NULL, tick_thread, NULL);
 
-    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(COLOR_GRAFIT1), LV_PART_MAIN);
+    /* Mode screen */
+    lv_obj_t *screen_mode = lv_screen_active();
+    lv_obj_set_style_bg_color(screen_mode, lv_color_hex(COLOR_GRAFIT), LV_PART_MAIN);
+    lv_obj_t *label_mode = lv_label_create(screen_mode);
+    lv_label_set_text(label_mode, "Main screen");
+    lv_obj_set_style_text_color(label_mode, lv_color_hex(COLOR_TEXT), LV_PART_MAIN);
+    lv_obj_align(label_mode, LV_ALIGN_TOP_MID, 0, 0);
+    lv_obj_set_style_text_font(label_mode, &lv_font_montserrat_30, 0);
 
-    lv_obj_t * label = lv_label_create(lv_screen_active());
-    lv_label_set_text(label, "Main_2");
-    lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(COLOR_TEXT), LV_PART_MAIN);
-    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);
+    /* Menu screen */
+    lv_obj_t *screen_menu = lv_obj_create(NULL);
+    lv_obj_set_style_bg_color(screen_menu, lv_color_hex(COLOR_GRAFIT), LV_PART_MAIN);
+    lv_obj_t *label_menu = lv_label_create(screen_menu);
+    lv_label_set_text(label_menu, "Menu");
+    lv_obj_set_style_text_color(label_menu, lv_color_hex(COLOR_TEXT), LV_PART_MAIN);
+    lv_obj_align(label_menu, LV_ALIGN_TOP_MID, 0, 0);
+
+    // Data screen
+    lv_obj_t *screen_data = lv_obj_create(NULL);
+    lv_obj_set_style_bg_color(screen_data, lv_color_hex(COLOR_GRAFIT), LV_PART_MAIN);
+    lv_obj_t *label_data = lv_label_create(screen_data);
+    lv_label_set_text(label_data, "data screen");
+    lv_obj_set_style_text_color(label_data, lv_color_hex(COLOR_TEXT), LV_PART_MAIN);
+    lv_obj_align(label_data, LV_ALIGN_TOP_MID, 0, 0);
 
     struct keyboard kb;
     if (keyboard_init(&kb) != 0) {
-        lv_label_set_text(label, "fpga open error");
+        lv_label_set_text(label_mode, "fpga open error");
         return 1;
     }
-    char str_number[20]; // Буфер для строки
-
     
     while (1)
     {
         lv_timer_handler();
         keyboard_poll(&kb);
+        if (kb.state.mode) lv_screen_load(screen_mode);
+        if (kb.state.menu) lv_screen_load(screen_menu);
+        if (kb.state.data) lv_screen_load(screen_data);
         usleep(5000);
     }
     keyboard_deinit(&kb);
