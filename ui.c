843331bee9bb25c1e5ed7f031b77506d806aab5a
Diconay <iesreg@ya.ru>
2025-08-29 14:59:49 +0700

    add power scale

--- ui.c
+++ ui.c
@@ -2,37 +2,66 @@
 #include "fonts/castom_fonts.h"
 
 #define COLOR_GRAFIT   0x0B1E3A
-#define COLOR_TEXT     0x8f8f8f
+#define COLOR_TEXT     0xFFFFFF
 
 static lv_obj_t * needle_line;
 
 static void set_needle_line_value(void * obj, int32_t v)
 {
-    lv_scale_set_line_needle_value((lv_obj_t *)obj, needle_line, 60, v);
+    lv_scale_set_line_needle_value((lv_obj_t *)obj, needle_line, 102, v);
 }
 
 void power_scale(lv_obj_t *screen){
     lv_obj_t * scale_line = lv_scale_create(screen);
-    lv_obj_set_size(scale_line, 150, 150);
-    lv_scale_set_mode(scale_line, LV_SCALE_MODE_ROUND_INNER);
-    lv_obj_set_style_bg_opa(scale_line, LV_OPA_COVER, 0);
-    lv_obj_set_style_bg_color(scale_line, lv_palette_lighten(LV_PALETTE_RED, 5), 0);
-    lv_obj_set_style_radius(scale_line, LV_RADIUS_CIRCLE, 0);
-    lv_obj_set_style_clip_corner(scale_line, true, 0);
-    lv_obj_align(scale_line, LV_ALIGN_LEFT_MID, LV_PCT(2), 0);
+    lv_obj_set_size(scale_line, 230, 230);
+    lv_scale_set_mode(scale_line, LV_SCALE_MODE_ROUND_OUTER);
+    lv_obj_set_style_bg_opa(scale_line, LV_OPA_TRANSP, 0);
+    lv_obj_align(scale_line, LV_ALIGN_CENTER, LV_PCT(2), 0);
 
     lv_scale_set_label_show(scale_line, true);
+    lv_obj_set_style_text_color(scale_line, lv_color_white(), LV_PART_INDICATOR);
+    lv_obj_set_style_line_color(scale_line, lv_color_white(), LV_PART_ITEMS);
+    lv_obj_set_style_line_color(scale_line, lv_color_white(), LV_PART_INDICATOR);
     lv_scale_set_total_tick_count(scale_line, 31);
-    lv_scale_set_major_tick_every(scale_line, 5);
+    lv_scale_set_major_tick_every(scale_line, 6);
     lv_obj_set_style_length(scale_line, 5, LV_PART_ITEMS);
     lv_obj_set_style_length(scale_line, 10, LV_PART_INDICATOR);
-    lv_scale_set_range(scale_line, 10, 40);
+    lv_scale_set_range(scale_line, 0, 1250);
 
-    lv_scale_set_angle_range(scale_line, 270);
-    lv_scale_set_rotation(scale_line, 135);
+    lv_scale_set_angle_range(scale_line, 225);
+    lv_scale_set_rotation(scale_line, 180);
 
+    lv_obj_set_style_arc_width(scale_line, 6, LV_PART_MAIN);
+    lv_obj_set_style_arc_color(scale_line, lv_color_white(), LV_PART_MAIN);
+
+    lv_obj_t *seg_green = lv_arc_create(scale_line);
+    lv_obj_remove_style_all(seg_green);
+    lv_obj_set_size(seg_green, 200, 200);
+    lv_arc_set_bg_angles(seg_green, 180, 300);
+    lv_obj_set_style_arc_width(seg_green, 40, 0);
+    lv_obj_set_style_arc_color(seg_green, lv_palette_main(LV_PALETTE_GREEN), 0);
+    lv_obj_center(seg_green);
+
+    lv_obj_t *seg_yellow = lv_arc_create(scale_line);
+    lv_obj_remove_style_all(seg_yellow);
+    lv_obj_set_size(seg_yellow, 200, 200);
+    lv_arc_set_bg_angles(seg_yellow, 300, 0);
+    lv_obj_set_style_arc_width(seg_yellow, 40, 0);
+    lv_obj_set_style_arc_color(seg_yellow, lv_palette_main(LV_PALETTE_YELLOW), 0);
+    lv_obj_center(seg_yellow);
+
+    lv_obj_t *seg_red = lv_arc_create(scale_line);
+    lv_obj_remove_style_all(seg_red);
+    lv_obj_set_size(seg_red, 200, 200);
+    lv_arc_set_bg_angles(seg_red, 0, 45);
+    lv_obj_set_style_arc_width(seg_red, 40, 0);
+    lv_obj_set_style_arc_color(seg_red, lv_palette_main(LV_PALETTE_RED), 0);
+    lv_obj_center(seg_red);
+
+    needle_line = lv_line_create(scale_line);
     lv_obj_set_style_line_width(needle_line, 6, LV_PART_MAIN);
     lv_obj_set_style_line_rounded(needle_line, true, LV_PART_MAIN);
+    lv_obj_set_style_line_color(needle_line, lv_color_white(), LV_PART_MAIN);
     
     lv_anim_t anim_scale_line;
     lv_anim_init(&anim_scale_line);
@@ -41,7 +70,7 @@ void power_scale(lv_obj_t *screen){
     lv_anim_set_duration(&anim_scale_line, 1000);
     lv_anim_set_repeat_count(&anim_scale_line, LV_ANIM_REPEAT_INFINITE);
     lv_anim_set_reverse_duration(&anim_scale_line, 1000);
-    lv_anim_set_values(&anim_scale_line, 10, 40);
+    lv_anim_set_values(&anim_scale_line, 0, 1000);
     lv_anim_start(&anim_scale_line);
 }
 
